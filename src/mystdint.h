0fcc2b22668c584e7ed9ffafabe3a55b1322d8ff
Felipe Vieira <felipevieira58238@gmail.com>
2026-09-19 22:19:35 -0300

    feat:header com macros customizados do stdint

---
+++ src/mystdint.h
@@ -0,0 +1,18 @@
+/*
+	My own version of the stdint.h macros
+    by:Felipe Vieira
+	Last Update:19/08/2026
+*/
+
+#ifndef _MYSTDINT_H_
+#define _MYSTDINT_H_
+
+#include "stdint.h"
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+
+typedef int8_t i8;
+typedef int16_t i16;
+
+#endif
